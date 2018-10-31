ac8db9d7c6a1e80a0ac289cd339d2922218f5dfe
Jesse Johnson <jessejohnsonla@gmail.com>
2018-10-30 21:48:59 -0700

    commit

--- src/app/invoices/edit/edit.component.ts
+++ src/app/invoices/edit/edit.component.ts
@@ -1,15 +1,17 @@
 import { Invoice } from './../invoices.model';
 import { ActivatedRoute } from '@angular/router';
-import { Component, OnInit, ViewChild, ElementRef, Attribute } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Attribute, OnDestroy } from '@angular/core';
 import {deserialize, serialize, IGenericObject} from 'json-typescript-mapper';
 import { FormGroup, FormControl } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { InvoicesService } from '../invoices.service';
 
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
   styleUrls: ['./edit.component.css']
 })
-export class EditComponent implements OnInit {
+export class EditComponent implements OnInit, OnDestroy {
 
   id:number;
   mode:string = 'Create';
@@ -19,34 +21,43 @@ export class EditComponent implements OnInit {
   invoice:Invoice;
   @ViewChild('editinvoiceform') editinvoiceform;
   @ViewChild('savebutton') savebutton : ElementRef;
+  subscriptions: Subscription[] = [];
 
-  constructor(private route: ActivatedRoute) { 
+  constructor(private route: ActivatedRoute,
+              private invoicesservice: InvoicesService) { 
+  }
+
+  ngOnDestroy(): void {
+    for(let subscription of this.subscriptions){
+      subscription.unsubscribe();
+    }
   }
 
   ngOnInit() {
     this.id = this.route.snapshot.params['id'];
-    this.route.params.subscribe((params) => { this.id = params['id']});
-    
+    var sub = this.route.params.subscribe((params) => { this.id = params['id']});
+    this.subscriptions.push(sub);
+
     if(this.id > 0) {
       this.mode = 'Edit';
     }
     var $json:IGenericObject = {"BillToName":"bill-to-name"};
+    
+    this.invoicesservice.getInvoice(this.id).subscribe( invoice => {
+      this.invoice = invoice;
+    })
+
     //var invoice = deserialize(Invoice, $json);
-    this.invoice = new Invoice();
-    this.invoice.BillToName = 'bill-to-name';
-    $json = serialize(this.invoice);
-      
-    // this.editinvoiceform = new FormGroup({
-    //   BillToName: new FormControl('')
-    // });
+    // this.invoice = new Invoice();
+    // this.invoice.BillToName = 'bill-to-name';
+    // $json = serialize(this.invoice);
 
-    this.editinvoiceform.valueChanges
+    var sub:Subscription = this.editinvoiceform.valueChanges
         .subscribe(status => {
-          this.isdirty = true;
+          this.isdirty = this.editinvoiceform.dirty;
           this.setSaveDisableStatus(this.savebutton.nativeElement);
-          console.log(status);
         });
-
+    this.subscriptions.push(sub);
   }
 
   onToggleAddressClick(){
@@ -56,7 +67,22 @@ export class EditComponent implements OnInit {
 
   onSaveClick(elementRef)
   {
-    this.isdirty = false;
+    var sub:Subscription;
+    if(this.mode === 'Edit') {
+      sub = this.invoicesservice.updateInvoice(this.invoice).subscribe(invoice => {
+        this.isdirty = false;
+      });
+    }
+    else {
+      sub = this.invoicesservice.createInvoice(this.invoice).subscribe(invoice => {
+        if(invoice.success)
+          this.isdirty = false;
+          else{
+            alert(invoice.errormsg + '!!!');
+          }
+      });
+    }
+    this.subscriptions.push(sub);
     this.setSaveDisableStatus(elementRef.srcElement);
   }
 
--- src/app/invoices/invoices.service.ts
+++ src/app/invoices/invoices.service.ts
@@ -1,16 +1,55 @@
-import { Injectable, OnInit } from '@angular/core';
+import { Injectable, OnInit, OnDestroy } from '@angular/core';
 import { InvoiceItem, Invoice } from './invoices.model';
-import {HttpClient} from '@angular/common/http';
-import { Observable, of } from 'rxjs';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { Observable, of, Subscription } from 'rxjs';
+import { serialize } from 'json-typescript-mapper';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'text/plain'
+  })
+};
 
 @Injectable({
   providedIn: 'root'
 })
-export class InvoicesService {
+export class InvoicesService implements OnDestroy {
 
   baseUrl:string = 'http://localhost/';
+  putsubscription: Subscription;
+  postsubscription: Subscription;
 
   constructor(private httpClient: HttpClient) { }
+  
+  ngOnDestroy(): void {
+    if(!!this.putsubscription)
+      this.putsubscription.unsubscribe();
+      if(!!this.postsubscription)
+        this.postsubscription.unsubscribe();
+  }
+
+  createInvoice(invoice: Invoice): Observable<{success:boolean, errormsg: string}> {
+
+    var post = this.httpClient.post<Invoice>(this.baseUrl + 'invoice/', invoice);
+    var created:Invoice;
+
+    this.postsubscription = post.subscribe(invoice => {
+      created = invoice
+    },
+    error => { return new Observable<{success: false, errormsg: null}>() });
+    return new Observable<{success: true, errormsg: null}>();
+  }
+
+  updateInvoice(invoice: Invoice): Observable<{success:boolean, errormsg: string}>  {
+    var updated:Invoice;
+
+    var put = this.httpClient.post<Invoice>(this.baseUrl + 'invoice/', invoice, httpOptions);
+    this.putsubscription = put.subscribe(invoice => {
+      updated = invoice
+    },
+    error => { return new Observable<{success: false, errormsg: null}>() });
+    return new Observable<{success: true, errormsg: null}>();
+  }
 
   getInvoice(invoiceid: number): Observable<Invoice> {
     return this.httpClient.get<Invoice>(this.baseUrl + 'invoice/' + invoiceid);
